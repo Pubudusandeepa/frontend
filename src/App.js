4aac72c9a0935d6048cf2406d157f4638d796390
Pubudusandeepa <ppsandeepa12@gmail.com>
2023-03-02 19:53:38 +0530

    comit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. new year pubudu
+          Edit <code>src/App.js</code> a. new year pubudu
         </p>
         <a
           className="App-link"
